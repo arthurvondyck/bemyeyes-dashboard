235140fd06c574eaac2160b860828342fba8ba3f
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-08-11 09:11:56 +0200

    moving on

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -12,8 +12,9 @@ SCHEDULER.every '1m' do
   current_valuation = rand(100)
   current_karma     = rand(200000)
 
+
   send_event('valuation', { current: current_valuation, last: last_valuation })
-  send_event('karma', { current: current_karma, last: last_karma })
-  send_event('synergy',   { value: Token.count })
+  send_event('sighted_logged_in',   { value: Token.count })
+  send_event('blind_logged_in'), {value: 42}
   send_event('abuse_reports',   { value: AbuseReport.count })
 end
