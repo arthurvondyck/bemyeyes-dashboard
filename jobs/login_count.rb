4aa403b895ddca525e3a1cbb7b843d5cbb8aed5a
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-10-01 11:12:34 +0200

    connect to mongo at startup, cleanup

--- jobs/login_count.rb
+++ jobs/login_count.rb
@@ -5,13 +5,11 @@ current_karma = 0
 
 
 SCHEDULER.every '1m' do
-  MongoMapper.connection = Mongo::Connection.new("localhost")
-  MongoMapper.database = 'bemyeyes'
   blind_user_ids = User.where(:role => "blind").collect {|blind| blind._id}.flatten
   sighted_user_ids = User.where(:role => "helper").collect {|blind| blind._id}.flatten
 
   blind_user_count = Token.where(:user_id => {:$in =>blind_user_ids}).count
-   sighted_user_count = Token.where(:user_id => {:$in =>sighted_user_ids}).count
+  sighted_user_count = Token.where(:user_id => {:$in =>sighted_user_ids}).count
 
   send_event('sighted_logged_in',   { value: sighted_user_count })
   send_event('blind_logged_in', {value: blind_user_count})
