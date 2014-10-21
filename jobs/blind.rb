23509730f6ceed7df5b1f5d9729397ba9d1ce4a6
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-10-21 21:27:08 +0200

    added more counts

--- jobs/blind.rb
+++ jobs/blind.rb
@@ -5,10 +5,20 @@ SCHEDULER.every '1m', :first_in => 0 do
   db = MongoClient.new("localhost", 27017, w: 1).db("bemyeyes")
   coll = db.collection("requests")
 
-  blind_above_5_calls = coll.aggregate([
-  {"$group" => {_id: "$blind_id", count: {"$sum" => 1}}},
-  {"$match" => {count: {"$gte" => 5}}}
-]).count
 
-   send_event('blind_above_5_calls',   { value: blind_above_5_calls })
+  blind_above_1_calls = find_count_above 1
+  send_event('blind_above_1_calls',   { value: blind_above_1_calls })
+
+  blind_above_5_calls = find_count_above 5
+  send_event('blind_above_5_calls',   { value: blind_above_5_calls })
+
+  blind_above_10_calls = find_count_above 10
+  send_event('blind_above_10_calls',   { value: blind_above_10_calls })
+end
+
+def find_count_above(limit)
+  coll.aggregate([
+    {"$group" => {_id: "$blind_id", count: {"$sum" => 1}}},
+    {"$match" => {count: {"$gte" => limit}}}
+    ]).count
 end
