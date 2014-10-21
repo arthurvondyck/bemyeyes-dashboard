c867aebf044935573f226d47b61bd54c99b8386e
Klaus Hebsgaard <klaus@hebsgaard.dk>
2014-10-21 18:36:03 +0200

    aggregate counts

--- jobs/blind.rb
+++ jobs/blind.rb
@@ -3,10 +3,10 @@ include Mongo
 
 SCHEDULER.every '1m', :first_in => 0 do
   db = MongoClient.new("localhost", 27017, w: 1).db("bemyeyes")
-  coll = db.collection("users")
+  coll = db.collection("requests")
 
   blind_above_5_calls = coll.aggregate([
-  {"$group" => {_id: "$state", count: {"$sum" => 1}}},
+  {"$group" => {_id: "$blind_id", count: {"$sum" => 1}}},
   {"$match" => {count: {"$gte" => 5}}}
 ]).count
 
